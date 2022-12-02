df3208bc1250072e83a2c7253857ef5116ec0cec
MaryIshola <funmilayoishola72@gmail.com>
2022-12-01 21:07:47 -0800

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    """Print the addition of all arguments."""
-    import sys
-
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+    from sys import argv
+    i = 1
+    if len(argv) < 2:
+        print("0 arguments.")
+    elif len(argv) < 3:
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(len(argv) - 1))
+    for arg in argv[1:]:
+        print("{:d}: {:s}".format(i, arg))
+        i += 1
